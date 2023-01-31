58d76a3e76eca53377814681baa413a781c708fc
felipematheus1337 <lipehbr@gmail.com>
2023-01-31 20:17:35 -0300

    Authentication Controller finalizado, Serviço de Segurança com JWT finalizado!

---
+++ src/main/java/com/agenda/auth/controller/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.agenda.auth.controller;
+
+
+import com.agenda.auth.dto.AuthenticationRequest;
+import com.agenda.auth.dto.AuthenticationResponse;
+import com.agenda.auth.dto.RegisterRequest;
+import com.agenda.auth.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+
+
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+
+
+    }
+
+}
